01346df24bf3bd8286827433be4ee76e2c685087
amandaperrone <amanda_degrande@yahoo.com.br>
2021-03-17 14:24:24 -0300

    BFS

---
+++ aula_2021_03_16.py
@@ -0,0 +1,47 @@
+# returnar o caminho entre A e os vértices
+
+def BFS(G, s):
+    visitadosT= [s]
+
+    distLambda = {}
+    distLambda[s] = 0
+
+    filaQ = [s]
+
+    caminhos = {}
+    caminhos[s] = [s]
+
+
+    while len(filaQ) > 0:
+        u = filaQ.pop(0)
+
+        for v in G[u]:
+            if v not in visitadosT:
+                filaQ.append(v)
+                visitadosT.append(v)
+                distLambda[v] = distLambda[u] + 1
+                caminhos[v] = caminhos[u].copy() + [v]
+                # caminhos[v].append(v)
+
+
+
+    return visitadosT, distLambda, caminhos
+
+######################
+
+G = {
+  'A' : ['B','C','D'],
+  'B' : ['A', 'E'],
+  'C' : ['A','G'],
+  'D' : ['A','H'],
+  'E' : ['B'],
+  'F' : [],
+  'G' : ['C','H'],
+  'H' : ['D','G','I'],
+  'I' : ['H']
+}
+
+visitados, distancias, caminhos = BFS(G,'A')
+print("Visitados:", visitados)
+print("Distancias:", distancias)
+print("Caminhos:", caminhos)
